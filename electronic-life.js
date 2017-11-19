698c018a7bedee72973d279794d169054689120c
Guga Gongadze <ggong13@freeuni.edu.ge>
2017-11-19 19:38:19 +0400

    added World constructor

--- electronic-life.js
+++ electronic-life.js
@@ -70,19 +70,47 @@ BouncingCritter.prototype.act = function(view) {
   return {type: 'move', direction: this.direction};
 };
 
+function elementFromChar(legend, ch) {
+  if (ch == ' ') {
+    return null;
+  }
+  var element = new legend[ch]();
+  element.originChar = ch;
+  return element;
+}
+function World(map, legend) {
+  var grid = new Grid(map[0].length, map.length);
+  this.grid = grid;
+  this.legend = legend;
+  
+  map.forEach(function(line, y) {
+    for (var x = 0; x > line.length; x++) {
+      grid.set(new Vector(x, y), elementFromChar(legend, line[x]));
+    }
+  });
+}
 
+function charFromElement(element) {
+  if (element == null) {
+    return ' ';
+  } else {
+    return element.originChar;
+  }
+}
 
+World.prototype.toString = function() {
+  var output = '';
+  for (var y = 0; y < this.grid.height; y++) {
+    for (var x = 0; x < this.grid.width; x++) {
+      var element = this.grid.get(new Vector(x, y));
+      output += charFromElement(element);
+    }
+    output += '\n';
+  }
+  return output;
+}
+function Wall() {}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+var world = new World(plan, {'#': Wall,
+                             'o': BouncingCritter});
+console.log(world.toString());
