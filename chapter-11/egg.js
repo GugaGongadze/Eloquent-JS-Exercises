37b1c4b765bde8efe3aa66427599e17c260b745f
Guga Gongadze <ggong13@freeuni.edu.ge>
2017-11-29 16:57:05 +0400

    added imperative paradigm support

--- chapter-11/egg.js
+++ chapter-11/egg.js
@@ -1,10 +1,3 @@
-// do(define(x, 10),
-//     if(>(x, 5),
-//         print("large"),
-//         print("small")))
-
-
-
 function parseExpression(program) {
     program = skipSpace(program);
     var match, expr;
@@ -69,10 +62,8 @@ function parse(program) {
     return result.expr;
 }
 
-console.log(parse("+(a, 10)"));
-
 function evaluate(expr, env) {
-    switch(expr.type) {
+    switch (expr.type) {
         case "value":
             return expr.value;
 
@@ -90,10 +81,96 @@ function evaluate(expr, env) {
             var op = evaluate(expr.operator, env);
             if (typeof op != "function")
                 throw new TypeError("Applying a non-function.");
-            return op.apply(null, expr.args.map(function(arg) {
+            return op.apply(null, expr.args.map(function (arg) {
                 return evaluate(arg, env);
             }));
     }
 }
 
 var specialForms = Object.create(null);
+
+specialForms["if"] = function (args, env) {
+    if (args.length != 3)
+        throw new SyntaxError("Bad number of args to if");
+    if (evaluate(args[0], env) !== false)
+        return evaluate(args[1], env);
+    else
+        return evaluate(args[2], env);
+};
+
+specialForms["while"] = function (args, env) {
+    if (args.length != 2)
+        throw new SyntaxError("Bad number of args to while")
+    while (evaluate(args[0], env) !== false)
+        evaluate(args[1], env);
+
+    // Since undefined does not exist in Egg, we return false,
+    // for lack of a meaningful result.
+    return false;
+};
+
+specialForms["do"] = function (args, env) {
+    var value = false;
+    args.forEach(function (arg) {
+        value = evaluate(arg, env);
+    });
+    return value;
+};
+
+specialForms["define"] = function (args, env) {
+    if (args.length != 2 || args[0].type != "word")
+        throw new SyntaxError("Bad use of define");
+    var value = evaluate(args[1], env);
+    env[args[0].name] = value;
+    return value;
+};
+
+specialForms["fun"] = function(args, env) {
+    if (!args.length)
+        throw new SyntaxError("Functions need a body");
+    function name(expr) {
+        if (expr.type != "word")
+            throw new SyntaxError("arg names must be words");
+        return expr.name;
+    }
+    var argNames = args.slice(0, args.length - 1).map(name);
+    var body = args[args.length - 1];
+
+    return function() {
+        if (arguments.length != argNames.length)
+            throw new TypeError("Wrong number of arguments");
+        var localEnv = Object.create(env);
+        for (var i = 0; i <arguments.length; i++)
+            localEnv[argNames[i]] = arguments[i];
+        return evaluate(body, localEnv);
+    };
+};
+
+var topEnv = Object.create(null);
+
+topEnv["true"] = true;
+topEnv["false"] = false;
+
+["+", "-", "*", "/", "==", "<", ">"].forEach(function(op) {
+    topEnv[op] = new Function("a, b", "return a " + op + " b;");
+});
+
+topEnv["print"] = function(value) {
+    console.log(value);
+    return value;
+};
+
+function run() {
+    var env = Object.create(topEnv);
+    var program = Array.prototype.slice.call(arguments, 0).join("\n");
+    return evaluate(parse(program), env);
+}
+
+run("do(define(plusOne, fun(a, +(a, 1))),",
+    "   print(plusOne(10)))");
+
+// run("do(define(pow, fun(base, exp,",
+//     "     if(==(exp, 0),",
+//     "        1,",
+//     "        *(base, pow(base, -(exp, 1)))))),",
+//     "   print(pow(2, 10)))");
