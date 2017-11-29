b6e02ba45f0ad9008711f0b2446c04c102c42ffa
Guga Gongadze <ggong13@freeuni.edu.ge>
2017-11-29 12:58:38 +0400

    added evaluation function

--- chapter-11/egg.js
+++ chapter-11/egg.js
@@ -3,28 +3,30 @@
 //         print("large"),
 //         print("small")))
 
-// {
-//     type: "apply",
-//     operator: { type: "word", name: ">" },
-//     args: [
-//         { type: "word", name: "x" },
-//         { type: "value", value: 5 }
-//     ]
-// }
+
 
 function parseExpression(program) {
     program = skipSpace(program);
-    var match;
-    var expr;
-    if (match = /^"([^"]*)"/.exec(program)) {
-        expr = { type: "value", value: match[1] };
-    } else if (match = /^\d+\b/.exec(program)) {
-        expr = { type: "value", value: Number(match[0]) };
-    } else if (match = /^[^\s(),"]+/.exec(program)) {
-        expr = { type: "word", name: match[0] };
-    } else {
+    var match, expr;
+    if (match = /^"([^"]*)"/.exec(program))
+        expr = {
+            type: "value",
+            value: match[1]
+        };
+    else if (match = /^\d+\b/.exec(program))
+        expr = {
+            type: "value",
+            value: Number(match[0])
+        };
+    else if (match = /^[^\s(),"]+/.exec(program))
+        expr = {
+            type: "word",
+            name: match[0]
+        };
+    else
         throw new SyntaxError("Unexpected syntax: " + program);
-    }
+
+    return parseApply(expr, program.slice(match[0].length));
 }
 
 function skipSpace(string) {
@@ -35,22 +37,63 @@ function skipSpace(string) {
 
 function parseApply(expr, program) {
     program = skipSpace(program);
-    if (program[0] != "(") {
-        return { expr: expr, rest: program };
-    }
+    if (program[0] != "(")
+        return {
+            expr: expr,
+            rest: program
+        };
 
     program = skipSpace(program.slice(1));
-    expr = { type: "apply", oeprator: expr, args: [] };
+    expr = {
+        type: "apply",
+        operator: expr,
+        args: []
+    };
     while (program[0] != ")") {
         var arg = parseExpression(program);
         expr.args.push(arg.expr);
         program = skipSpace(arg.rest);
-        if (program[0] == ",") {
+        if (program[0] == ",")
             program = skipSpace(program.slice(1));
-        } else if (program[0] != ")") {
+        else if (program[0] != ")")
             throw new SyntaxError("Expected ',' or ')'");
-        }
-        return parseApply(expr, program.slice(1));
-        
+    }
+    return parseApply(expr, program.slice(1));
+}
+
+function parse(program) {
+    var result = parseExpression(program);
+    if (skipSpace(result.rest).length > 0) {
+        throw new SyntaxError("Unexpected text after program");
+    }
+    return result.expr;
+}
+
+console.log(parse("+(a, 10)"));
+
+function evaluate(expr, env) {
+    switch(expr.type) {
+        case "value":
+            return expr.value;
+
+        case "word":
+            if (expr.name in env)
+                return env[expr.name];
+            else
+                throw new ReferenceError("Undefined variable: " + expr.name);
+
+        case "apply":
+            if (expr.operator.type == "word" &&
+                expr.operator.name in specialForms)
+                return specialForms[expr.operator.name](expr.args, env);
+
+            var op = evaluate(expr.operator, env);
+            if (typeof op != "function")
+                throw new TypeError("Applying a non-function.");
+            return op.apply(null, expr.args.map(function(arg) {
+                return evaluate(arg, env);
+            }));
     }
 }
+
+var specialForms = Object.create(null);
