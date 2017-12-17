441fb896c3305867cbe50d254b73b19dbf34aaee
Guga Gongadze <ggong13@freeuni.edu.ge>
2017-12-17 15:53:43 +0400

    added drawing and erasing fucntionality

--- chapter-19-a-paint-program/script.js
+++ chapter-19-a-paint-program/script.js
@@ -17,7 +17,7 @@ function elt(name, attributes) {
 
 var controls = Object.create(null);
 
-function createpaint(parent) {
+function createPaint(parent) {
     var canvas = elt('canvas', {
         width: 500,
         height: 300
@@ -34,3 +34,60 @@ function createpaint(parent) {
     }, canvas);
     parent.appendChild(elt('div', null, panel, toolbar));
 }
+
+var tools = Object.create(null);
+
+controls.tool = function (cx) {
+    var select = elt('select');
+    for (var name in tools)
+        select.appendChild(elt('option', null, name));
+
+    cx.canvas.addEventListener('mousedown', function (event) {
+        if (event.which == 1) {
+            tools[select.value](event, cx);
+            event.preventDefault();
+        }
+    });
+
+    return elt('span', null, 'Tool: ', select);
+};
+
+function relativePos(event, element) {
+    var rect = element.getBoundingClientRect();
+    return {
+        x: Math.floor(event.clientX - rect.left),
+        y: Math.floor(event.clientY - rect.top)
+    };
+}
+
+function trackDrag(onMove, onEnd) {
+    function end(event) {
+        removeEventListener('mousemove', onMove);
+        removeEventListener('mouseup', end);
+        if (onEnd)
+            onEnd(event);
+    }
+    addEventListener('mousemove', onMove);
+    addEventListener('mouseup', end);
+}
+
+tools.Line = function (event, cx, onEnd) {
+    cx.lineCap = 'round';
+
+    var pos = relativePos(event, cx.canvas);
+    trackDrag(function (event) {
+        cx.beginPath();
+        cx.moveTo(pos.x, pos.y);
+        pos = relativePos(event, cx.canvas);
+        cx.lineTo(pos.x, pos.y);
+        cx.stroke();
+    }, onEnd);
+};
+tools.Erase = function (event, cx) {
+    cx.globalCompositeOperation = 'destination-out';
+    tools.Line(event, cx, function () {
+        cx.globalCompositeOperation = "source-over";
+    });
+};
+
+createPaint(document.body);
