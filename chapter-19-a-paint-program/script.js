1c127cdf1d3c722bae0e584f01a470f75adaa1ff
Guga Gongadze <ggong13@freeuni.edu.ge>
2017-12-17 17:48:05 +0400

    added solution to problem 01

--- chapter-19-a-paint-program/script.js
+++ chapter-19-a-paint-program/script.js
@@ -170,25 +170,62 @@ tools.Text = function (event, cx) {
         cx.fillText(text, pos.x, pos.y);
     }
 };
-tools.Spray = function(event, cx) {
+tools.Spray = function (event, cx) {
     var radius = cx.lineWidth / 2;
     var area = radius * radius * Math.PI;
     var dotsPerTick = Math.ceil(area / 30);
 
     var currentPos = relativePos(event, cx.canvas);
-    var spray = setInterval(function() {
+    var spray = setInterval(function () {
         for (var i = 0; i < dotsPerTick; i++) {
             var offset = randomPointInRadius(radius);
             cx.fillRect(currentPos.x + offset.x,
-                        currentPos.y + offset.y, 1, 1);
+                currentPos.y + offset.y, 1, 1);
         }
     }, 25);
-    trackDrag(function(event) {
+    trackDrag(function (event) {
         currentPos = relativePos(event, cx.canvas);
-    }, function() {
+    }, function () {
         clearInterval(spray);
     });
 };
+tools.Rectangle = function (event, cx) {
+    var currentPos = relativePos(event, cx.canvas);
+    var pageStart = {
+        x: event.pageX,
+        y: event.pageY
+    };
+
+    var trackingNode = document.createElement('div');
+    trackingNode.style.position = 'absolute';
+    trackingNode.style.background = cx.fillStyle;
+    document.body.appendChild(trackingNode);
+
+    trackDrag(function (event) {
+        var rect = rectangleFrom(pageStart, {
+            x: event.pageX,
+            y: event.pageY
+        });
+
+        trackingNode.style.left = rect.left + 'px';
+        trackingNode.style.top = rect.top + 'px';
+        trackingNode.style.width = rect.width + 'px';
+        trackingNode.style.height = rect.height + 'px';
+    }, function (event) {
+        var rect = rectangleFrom(currentPos, relativePos(event, cx.canvas));
+        cx.fillRect(rect.left, rect.top, rect.width, rect.height);
+        document.body.removeChild(trackingNode);
+    });
+};
+
+function rectangleFrom(a, b) {
+    return {
+        left: Math.min(a.x, b.x),
+        top: Math.min(a.y, b.y),
+        width: Math.abs(a.x - b.x),
+        height: Math.abs(a.y - b.y)
+    };
+}
 
 function randomPointInRadius(radius) {
     for (;;) {
